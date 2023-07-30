8cf3b174384a4b29c62aaa933e385f6b482f2846
a2en <ameenahsan619@gmail.com>
2023-07-30 15:28:59 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(error => console.error('Error connecting to MongoDB', error)));
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => { console.log('Connected to MongoDB') });
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+})
