fed1640061868b735df49b33fc973e616f95cb28
a2en <ameenahsan619@gmail.com>
2023-07-30 15:51:25 +0530

    Closes #3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }
 .then(() => console.log('Connected to MongoDB'))
